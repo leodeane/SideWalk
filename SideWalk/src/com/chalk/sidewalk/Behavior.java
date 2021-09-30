d67c161869c0da9a6e88d1b75f503979a6606bb8
leodeane <91691860+leodeane@users.noreply.github.com>
2021-09-30 12:58:17 -0500

    Update Behavior.java

--- SideWalk/src/com/chalk/sidewalk/Behavior.java
+++ SideWalk/src/com/chalk/sidewalk/Behavior.java
@@ -3,6 +3,8 @@ package com.chalk.sidewalk;
 public class Behavior
 {
 
-
+  //behavior needs to be an object that can be tied to the illustrations
+  //red light, green light, yellow light, no behavior recorded
+  //
 
 }
